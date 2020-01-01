9ceaea7a9380b900a83cb20fc48473f2872a06c5
yuuxhub <yk.imp11@gmail.com>
2020-01-01 14:03:25 +0900

    ファイル追加

---
+++ todolist.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShareNow</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <a href="http://localhost/ChiSkTrans/main.html">
+          <img class="logo" src="http://localhost/img/logo_yoko_resize.png">
+        </a>
+        </div>
+        <!--   -->
+        <div>
+          <span class="fa fa-bars menu-icon"></span>
+        </div>
+
+        <div class="header-right">
+          <a href="todolist.php">TodoList</a>
+          <a href="search.php">Search</a>
+          <a href="input.php">Input</a>
+          <a href="#" class="login">Login</a>
+        </div>
+      </div>
+    </header>
+
+<footer>
+    <div class="container">
+      <img src="http://localhost/img/logo_resize.png">
+    </div>
+  </footer>
+</body>
+</html>
